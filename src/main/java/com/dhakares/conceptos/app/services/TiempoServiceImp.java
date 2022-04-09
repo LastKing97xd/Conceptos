65f5b72bf419ab2a6a0a5c3db97c860e75b0c1dc
DhakAres <davidanthonyhidalgo@gmail.com>
2022-04-08 23:34:54 -0500

    Llenado de Metodo
    Se llenaron los metodos de la implementación y se coloco la anotación service

--- src/main/java/com/dhakares/conceptos/app/services/TiempoServiceImp.java
+++ src/main/java/com/dhakares/conceptos/app/services/TiempoServiceImp.java
@@ -3,32 +3,39 @@ package com.dhakares.conceptos.app.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dhakares.conceptos.app.dao.TiempoDao;
 import com.dhakares.conceptos.app.documents.Tiempo;
 
+
+@Service
 public class TiempoServiceImp implements TiempoService{
 
+	@Autowired
+	private TiempoDao tiempoDao;
+	
 	@Override
 	public List<Tiempo> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return tiempoDao.findAll();
 	}
 
 	@Override
-	public Optional<Tiempo> findById(String id) {
-		// TODO Auto-generated method stub
-		return null;
+	public Optional<Tiempo> findById(String id) {	
+		return tiempoDao.findById(id);
 	}
 
 	@Override
 	public void save(Tiempo tiempo) {
-		// TODO Auto-generated method stub
 		
+		tiempoDao.save(tiempo);
 	}
 
 	@Override
 	public void delete(Tiempo tiempo) {
-		// TODO Auto-generated method stub
 		
+		tiempoDao.delete(tiempo);
 	}
 	
 	
